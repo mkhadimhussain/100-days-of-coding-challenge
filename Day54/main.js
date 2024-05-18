81b0ce0b2c58f5a352e80b614bc3e3f204b5c01a
mkhadimhussain <163546834+mkhadimhussain@users.noreply.github.com>
2024-05-18 15:59:38 +0500

    100-days-coding-challenge

--- Day54/main.js
+++ Day54/main.js
@@ -1,110 +1,83 @@
 /////////////////// DAY 54 CHALLENGE ///////////////////
-
 // Declaration
-let code: string 
+var code;
 // Assigning value
 code = "TypeScript Coding";
 console.log(code);
 //--------------------------------------------------------------------------
-
 //To be concise, we can combine the variable declaration and assignment into a single line:
-let hello: string = "Hello, World!";
+var hello = "Hello, World!";
 // Declaration        Assignment 
 console.log(hello);
 //--------------------------------------------------------------------------
-
 // We can also declare multiple variables in one line:
-let user: string = "Alice", age: number = 23, message: string = "Hello";
-console.log(user,age,message);
+var user = "Alice", age = 23, message = "Hello";
+console.log(user, age, message);
 //--------------------------------------------------------------------------
-
 // That might seem shorter, but we don’t recommend it. For the sake of better readability, 
 // please use a single line per variable.
 // The multiline variant is a bit longer, but easier to read:
-let user1: string = "Saif";
-let age1: number = 21;
-let message1: string = "Hi"; 
-console.log(user1,age1,message1);
+var user1 = "Saif";
+var age1 = 21;
+var message1 = "Hi";
+console.log(user1, age1, message1);
 //--------------------------------------------------------------------------
-
 // Some people also define multiple variables in this multiline style:
-let user2: string = "Adil", 
-    age2: number = 32,
-    message2: string = "Hi Dear";
-console.log(user2,age2,message2);
+var user2 = "Adil", age2 = 32, message2 = "Hi Dear";
+console.log(user2, age2, message2);
 //--------------------------------------------------------------------------
-
 // …Or even in the “comma-first” style:
-let user3: string = "Moiz"
-    , age3: number = 24
-    , message3: string = "Hey, how are you?";
-console.log(user3,age3,message3);
+var user3 = "Moiz", age3 = 24, message3 = "Hey, how are you?";
+console.log(user3, age3, message3);
 //--------------------------------------------------------------------------
-
 // We can also change it as many times as we want:
-
-let fruit: string = "Apple";
-fruit = "Grapes";    // Value Change
+var fruit = "Apple";
+fruit = "Grapes"; // Value Change
 console.log(fruit);
 //--------------------------------------------------------------------------
-
-let learn: string = "I am Learning TypeScript";
-let messages: string;
+var learn = "I am Learning TypeScript";
+var messages;
 messages = learn;
-
-console.log(learn);    // I am Learning TypeScript
+console.log(learn); // I am Learning TypeScript
 console.log(messages); // I am Learning TypeScript
 //--------------------------------------------------------------------------
-
 // A repeated declaration of the same variable is an error:
-let happy: string = "I am Happy";
+var happy = "I am Happy";
 // let happy: string = "Error";   (Shows Error)
 //--------------------------------------------------------------------------
-
 // Variable Names
-let userName: string = "Yasir";
-let test123: string = "Hello";
-let $ : number = 1;
-let  _: number = 2;
+var userName = "Yasir";
+var test123 = "Hello";
+var $ = 1;
+var _ = 2;
 console.log($ + _);
-
 // Examples of incorrect variable names: (Shows Error)
 // let 3a: string = "Karachi";      (cannot start with a digit)
 // let my-name: string = "Alice";   (hyphens '-' aren't allowed in the name)
 //--------------------------------------------------------------------------
-
 // There is a list of reserved words, which cannot be used as variable names 
 // because they are used by the language itself.
 // For example: let, class, return, and function are reserved.
 // let let: number = 5;       // Error
 // let return: number = 5;    // Error
- 
 // num: number = 7;  // Error
-
 //--------------------------------------------------------------------------
-
 // CONSTANT
 // To declare a constant (unchanging) variable, use const instead of let:
-
-const myBirthday: string = '18.04.1982';
+var myBirthday = '18.04.1982';
 //--------------------------------------------------------------------------
-
 // TASKS 1
- let admin: string
- let namee: string
-
- namee = "John";
- admin = namee;
- console.log(admin);    // Outputs: John
+var admin;
+var namee;
+namee = "John";
+admin = namee;
+console.log(admin); // Outputs: John
 //--------------------------------------------------------------------------
-
 // TASK 2
-let Earth: string = "Our Planet";
-let currentUser: string = "Alice";
+var Earth = "Our Planet";
+var currentUser = "Alice";
 //--------------------------------------------------------------------------
-
 // TASK 3
-const BIRTHDAY: string = "24.07.2000";
-const personAge: number = 23;
+var BIRTHDAY = "24.07.2000";
+var personAge = 23;
 //--------------------------------------------------------------------------
-
---
+++ Day55/main.js
@@ -0,0 +1,94 @@
+"use strict";
+/////////////////// DAY 55 CHALLENGE ///////////////////
+Object.defineProperty(exports, "__esModule", { value: true });
+// DATA TYPES
+// Numbers
+let num = 3;
+// NaN represents a computational error. It is a result of an incorrect 
+// or an undefined mathematical operation.
+let k;
+// connsole.log( "not a number" / 2 );   shows NaN
+//console.log(k/2);   // Outputs: NaN
+console.log(NaN + 1); // Outputs: NaN
+console.log(3 * NaN); // Outputs: NaN
+//console.log(k/2-1);      // Outputs: NaN
+// So, if there’s a NaN somewhere in a mathematical expression, it propagates
+// to the whole result (there’s only one exception to that: NaN ** 0 is 1).
+console.log(NaN ** 0); // Outputs: 1
+console.log(NaN ** 1); // Outputs: NaN
+//----------------------------------------------------------------------------
+// BigInt
+// The “number” type cannot safely represent integer values larger than (253-1) 
+// (that’s 9007199254740991), or less than -(253-1) for negatives.
+// For example, these two numbers (right above the safe range) are the same:
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
+// BigInt type represent integers of arbitrary length.
+// A BigInt value is created by appending n to the end of an integer:
+// the "n" at the end means it's a BigInt
+const myNum = 1234567890123456789012345678901234567890n;
+console.log(myNum);
+//----------------------------------------------------------------------------
+// String
+// A string in JavaScript must be surrounded by quotes.
+// Double quotes: "Hello".
+// Single quotes: 'Hello'.
+// Backticks: `Hello`.
+let str1 = "Hello";
+let str2 = 'World';
+let str3 = `I write ${str1} ${str2}.`;
+let a = `Result ${2 + 3}`; // Result 5
+//----------------------------------------------------------------------------
+// Boolean (logical type)
+// The boolean type has only two values: true and false.
+// This type is commonly used to store yes/no values: true means “yes, correct”, 
+// and false means “no, incorrect”.
+let nameFieldChecked = true; // yes, name field is checked
+let ageFieldChecked = false; // no, age field is not checked
+// Boolean values also come as a result of comparisons:
+console.log(5 > 3); // Outputs: true
+//----------------------------------------------------------------------------
+// The “null” value
+// The special null value does not belong to any of the types described above.
+// It forms a separate type of its own which contains only the null value:
+let age = null;
+console.log(age); // null
+// It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+// The code above states that age is unknown.
+//----------------------------------------------------------------------------
+// The “undefined” value
+// The special value undefined also stands apart. It makes a type of its own, just like null.
+// The meaning of undefined is “value is not assigned”.
+// If a variable is declared, but not assigned, then its value is undefined:
+let myName;
+//console.log(myName);   // Shows undefined
+// Technically, it is possible to explicitly assign undefined to a variable:
+let myAge = undefined;
+console.log(myAge); // undefined
+//----------------------------------------------------------------------------
+// Objects
+// The object type is special.
+// All other types are called “primitive” because their values can contain only
+// a single thing (be it a string or a number or whatever). In contrast, objects
+// are used to store collections of data and more complex entities.
+//----------------------------------------------------------------------------
+// The typeof operator
+// The typeof operator returns the type of the operand. It’s useful when we want
+// to process values of different types differently or just want to do a quick check.
+// A call to typeof x returns a string with the type name:
+console.log(typeof undefined); // Output: undefined
+console.log(typeof 0); // Output: number
+console.log(typeof true); // Output: boolean
+console.log(typeof "Hello"); // Output: string
+console.log(typeof Symbol("id")); // Output: symbol
+console.log(typeof Math); // Output: object
+// Math is a built-in object that provides mathematical operations.
+console.log(typeof null); // Output: object
+// Definitely, null is not an object. It is a special value with a separate type of 
+// its own. The behavior of typeof is wrong here.
+let x = "Karachi";
+console.log(typeof (x)); // Output: string
+console.log(typeof x);
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
